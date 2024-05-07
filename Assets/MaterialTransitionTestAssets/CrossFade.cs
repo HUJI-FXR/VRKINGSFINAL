f7358605504550d8f93f22c752ddd1c02b892f57
Jonathan Miroshnik <fistuk2009@gmail.com>
2024-05-07 16:49:19 +0300

    Created clear Texture array for fade in Transition

--- Assets/MaterialTransitionTestAssets/CrossFade.cs
+++ Assets/MaterialTransitionTestAssets/CrossFade.cs
@@ -5,33 +5,46 @@ using UnityEngine.XR.Interaction.Toolkit.AffordanceSystem.Receiver.Primitives;
 public class CrossFade : MonoBehaviour
 {
     private Renderer renderer;
-    private float duration = 5f;
-    private float changeTextureEvery = 5f;
+    private float duration = 2f;
+    //private float changeTextureEvery = 2f;
 
     public Texture[] textures;
-    private int texNum = 1;
+    private int texNum = 0;
+
+    private float lerp = 0f;
 
     void Start()
     {
         renderer = GetComponent<Renderer>();
         renderer.material.SetFloat("_Blend", 0f);
 
-        InvokeRepeating("CrossFadeStart", changeTextureEvery, 1f);
+        //InvokeRepeating("CrossFadeStart", Time.time, duration);
     }
 
     void Update()
     {
-        float lerp = Mathf.PingPong(Time.time, duration) / duration;
+        lerp += Time.deltaTime / duration;
         renderer.material.SetFloat("_Blend", lerp);
+
+        if (lerp > 1)
+        {
+            CrossFadeStart();
+        }
     }
 
     public void CrossFadeStart()
     {
-        CrossFadeBetween(textures[texNum-1], textures[texNum]);
+        lerp = 0;
+        //CrossFadeBetween(textures[texNum-1], textures[texNum]);
 
         texNum++;
-        if (texNum >= textures.Length) {
-            texNum = 1;
+        if (texNum < textures.Length) {
+            CrossFadeBetween(textures[texNum - 1], textures[texNum]);
+        }
+        else
+        {
+            texNum = 0;
+            CrossFadeBetween(textures[textures.Length-1], textures[texNum]);
         }
     }
 
