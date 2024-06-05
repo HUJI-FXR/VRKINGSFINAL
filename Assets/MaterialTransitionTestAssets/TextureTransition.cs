883732424534396b65373c83c75357d31d0746b5
slemanaweiwi <slemanaweiwi@gmail.com>
2024-06-05 17:54:03 +0300

    textures transitions !!

---
+++ Assets/MaterialTransitionTestAssets/TextureTransition.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureTransition : MonoBehaviour
+{
+    public Material transitionMaterial;
+    public List<Texture> textures;
+    public float transitionSpeed = 1f;
+    [Range(0, 1)]
+    public float noiseIntensity = 1f;
+    [Range(0.01f, 1f)]
+    public float smoothness = 0.5f;
+
+    private float transition = 0f;
+    private int currentTextureIndex = 0;
+    private int nextTextureIndex = 1;
+
+    void Start()
+    {
+        if (textures.Count < 2)
+        {
+            Debug.LogError("You need at least two textures to perform a transition.");
+            enabled = false;
+            return;
+        }
+
+        // Initialize the first two textures
+        transitionMaterial.SetTexture("_CurrentTex", textures[currentTextureIndex]);
+        transitionMaterial.SetTexture("_NextTex", textures[nextTextureIndex]);
+    }
+
+    void Update()
+    {
+        // Animate the transition value
+        transition += Time.deltaTime * transitionSpeed;
+        if (transition > 1.0f)
+        {
+            transition = 0f;
+            currentTextureIndex = nextTextureIndex;
+            nextTextureIndex = (nextTextureIndex + 1) % textures.Count;
+
+            // Update the textures in the shader
+            transitionMaterial.SetTexture("_CurrentTex", textures[currentTextureIndex]);
+            transitionMaterial.SetTexture("_NextTex", textures[nextTextureIndex]);
+        }
+
+        transitionMaterial.SetFloat("_Transition", transition);
+        transitionMaterial.SetFloat("_NoiseIntensity", noiseIntensity);
+        transitionMaterial.SetFloat("_Smoothness", smoothness);
+    }
+}
